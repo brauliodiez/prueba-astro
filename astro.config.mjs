ecf8baaf74c3f01e5c079ce9b91dacdf0e2270cc
Braulio <braulio.diez@gmail.com>
2024-11-02 12:32:25 +0100

    test production

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://brauliodiez.github.io",
-  base: "/prueba-astro/",
+  base: process.env.NODE_ENV === "production" ? "/prueba-astro/" : "/",
 });
